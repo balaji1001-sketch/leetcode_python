f850f3cf64f64caba017412a3a2fc8d6dd62e8a6
balaji1001-sketch <balajijayakumar00031@gmail.com>
2026-02-09 21:18:57 +0530

    changes

---
+++ binarysearchtree.py
@@ -0,0 +1,30 @@
+class Solution:
+    def balanceBST(self, root):
+        arr = []
+        
+        class TreeNode:
+         def __init__(self, val=0, left=None, right=None):
+          self.val = val
+          self.left = left
+          self.right = right
+
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            arr.append(node.val)
+            inorder(node.right)
+
+        def build(l, r):
+            if l > r:
+                return None
+
+            mid = (l + r) // 2
+            node = TreeNode(arr[mid])
+            node.left = build(l, mid - 1)
+            node.right = build(mid + 1, r)
+            return node
+
+        inorder(root)
+        return build(0, len(arr) - 1)
